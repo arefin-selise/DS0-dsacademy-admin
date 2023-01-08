d91efa64e4a86116941401006b5421f16d50097b
arefin-selise <arefin.hossain@selise.ch>
2023-01-08 16:20:24 +0600

    init

---
+++ src/test/java/com/dailystar/dsacademy/DsacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailystar.dsacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
