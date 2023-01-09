b2584da3e58748cd930ee0624ebf0441db3407d5
mdPearEhabib <peare.habib@selise.ch>
2023-01-09 12:37:09 +0600

    connect with mongodb

--- src/main/java/com/dailystar/dsacademy/repository/UserRepository.java
+++ src/main/java/com/dailystar/dsacademy/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.dailystar.dsacademy.repository;
 
+import com.dailystar.dsacademy.model.User;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Lazy
-public interface UserRepository {
+public interface UserRepository extends MongoRepository<User, String> {
 
 }
--- src/main/java/com/dailystar/dsacademy/service/impl/UserServiceImpl.java
+++ src/main/java/com/dailystar/dsacademy/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.dailystar.dsacademy.service.impl;
 
 import com.dailystar.dsacademy.dto.request.RegistrationDto;
 import com.dailystar.dsacademy.model.User;
+import com.dailystar.dsacademy.repository.UserRepository;
 import com.dailystar.dsacademy.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Lazy;
@@ -13,19 +14,25 @@ import java.time.LocalDate;
 @Service
 @Lazy
 public class UserServiceImpl implements UserService {
-    public UserServiceImpl(){
 
+    private final UserRepository userRepository;
+    public UserServiceImpl(@Lazy UserRepository userRepository){
+        this.userRepository=userRepository;
     }
 
     @Override
     public User updateUserRegistration(final RegistrationDto request) {
         final User user = new User();
-        user.setFirstName(request.getFirstName());
-        user.setLastName(request.getLastName());
+        user.setFirstName("md");
+        user.setLastName("habib");
         user.setDateOfBirth(LocalDate.now());
-        user.setGender(request.getGender());
-        user.setEmail(request.getEmail());
-        user.addRole(request.getRole());
+        user.setGender("MALE");
+        user.setEmail("habib@gmail.com");
+        user.addRole("user");
+        user.setMobile("01741222333");
+        user.setLanguage("english");
+
+        userRepository.save(user);
         return user;
     }
 }
